c47e285207e2f65451f64e56471303fb31590b08
Gabriel Mazzuco <gabrielalvesmazzuco@hotmail.com>
2022-12-02 20:38:10 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+# Sumário:
+    # Matriz_cenario:
+        # 0  = caminho livre
+        # -1 = barcos
+        # 15 = luffy
+        # 99 = one_piece
+        
+    # Matriz_valores:
+        # 700.0 = barcos
+        # 800.0 = caminhos já percorridos
+        # 999.9 = void do mapa -> "valor teorico que não aparece de fato no mapa"
+        
+
 import numpy as np
 import math
 import random
 import os
+import sys
 
 class Luffy:
     def __init__(self, x, y):
@@ -17,6 +31,7 @@ class Navio:
     def __init__(self, x, y, matriz_cenario):
         for i in range(Navio.Barcos()):
             matriz_cenario[random.randint(0, x-1), random.randint(0, y-1)] = -1
+            
         
     # Define quantos barcos apareceram no mapa
     def Barcos():
@@ -33,41 +48,87 @@ def calculaEstimativa(posicaoX, posicaoY):
         medida3 = (pow(medida1, 2)) + (pow(medida2, 2))
         estimativa = math.sqrt(medida3)
         return estimativa
+    
+def ordena(fila_valores, fila_posicoes):
+    #https://pt.stackoverflow.com/questions/507769/ordenar-duas-listas-com-base-na-ordem-da-primeira
+    x = fila_valores
+    y = fila_posicoes
 
-def posicaoPersonagem(x, y, fila, posicaoX, posicaoY):    
+    # Ordene os índices em vez dos elementos em si
+    indices = list(range(len(x)))
+    
+    # Ordene os índices com relação ao seu respectivo valor em x
+    indices.sort(key=lambda i: x[i]) 
+
+    # Crie as listas baseado na ordem dos índices
+    new_x = [x[i] for i in indices]
+    new_y = [y[i] for i in indices]
+
+    return new_x, new_y
+
+def posicaoPersonagem(fila_posicoes, posicaoX, posicaoY):
     aux = posicaoX - 1, posicaoY 
-    if(posicaoX <= -1):
-        fila.append("Invalido")
-    else:
-        fila.append(aux) # Esquerda
+    fila_posicoes.append(aux) # Cima
             
     aux = posicaoX + 1, posicaoY
-    if(posicaoX >= x+1):
-        fila.append("Invalido")
-    else:
-        fila.append(aux) # Direita
+    fila_posicoes.append(aux) # Baixo
             
     aux = posicaoX, posicaoY - 1
-    if(posicaoY <= -1):
-        fila.append("Invalido")
-    else:
-        fila.append(aux) # Cima
+    fila_posicoes.append(aux) # Esquerda
             
     aux = posicaoX, posicaoY + 1
-    if(posicaoY >= y+1):
-        fila.append("Invalido")
-    else:
-        fila.append(aux) # Baixo
-    
-    print(fila)
-    return fila
+    fila_posicoes.append(aux) # Direita
 
-def custoMovimentacao(fila, fila_aux, matriz):
-    for i in range(len(fila)):
-        aux = matriz[fila[i][0], fila[i][1]]
-        fila_aux.append(aux)
-        
-    return fila_aux
+    return fila_posicoes
+
+def movimentacaoPersonagem(matriz, fila_posicoes, fila_valores, posicaoX, posicaoY):
+    # Cima
+    if(fila_posicoes[0][0] == -1):
+        aux = 999.9
+        fila_valores.append(aux)
+    else:
+        aux = matriz[posicaoX - 1, posicaoY]
+        fila_valores.append(aux)
+    
+    # Baixo
+    if(fila_posicoes[1][0] == x):
+        aux = 999.9
+        fila_valores.append(aux)	
+    else:
+        aux = matriz[posicaoX + 1, posicaoY]
+        fila_valores.append(aux)
+
+    # Esquerda
+    if(fila_posicoes[2][0] == -1):
+        aux = 999.9
+        fila_valores.append(aux)
+    else:
+        aux = matriz[posicaoX, posicaoY - 1]
+        fila_valores.append(aux)
+       
+    # Direita   
+    if(fila_posicoes[3][1] == y):
+        aux = 999.9
+        fila_valores.append(aux)
+    else:
+        aux = matriz[posicaoX, posicaoY + 1]
+        fila_valores.append(aux) 
+    
+    return fila_valores
+
+def printaTerminal(personagem_posicaoX, personagem_posicaoY, tesouro_posicaoX, tesouro_posicaoY,
+                   fila_valores, fila_posicoes, matriz, matriz_cenario):
+    
+    print('Perosnagem: ', personagem_posicaoX, personagem_posicaoY)
+    print('Tesouro: ' , tesouro_posicaoX, tesouro_posicaoY)
+    print('Fila com valores: ', fila_valores)
+    print('Fila com as posições: ', fila_posicoes)
+    print('\n')
+    print(matriz)
+    print('\n')
+    print(matriz_cenario)
+    print("--------------------------------------------------------------")
+    print('\n')
 
 def escreveArquivo(x, y, matriz, matriz_cenario, caminho, personagemX, personagemY, tesouroX, tesouroY):
     arquivo = open("resposta.txt", "w")
@@ -76,6 +137,16 @@ def escreveArquivo(x, y, matriz, matriz_cenario, caminho, personagemX, personage
     arquivo.write("Posição do Personagem: "+ str(personagemX) + " " + str(personagemY) + "\n")
     arquivo.write("Posição do Tesouro: "+ str(tesouroX) + " " + str(tesouroY) + "\n\n")
     
+    # Matriz de cenario
+    arquivo.write("Matriz de Cenário: \n")
+    for i in range(x):
+        for j in range(y):
+            arquivo.write(str(matriz_cenario[i, j]))
+            arquivo.write(" ")
+        arquivo.write("\n")
+    
+    arquivo.write("\n\n")
+    
     # Matriz de custo
     arquivo.write("Matriz de Custo: \n")
     for i in range(x):
@@ -83,17 +154,7 @@ def escreveArquivo(x, y, matriz, matriz_cenario, caminho, personagemX, personage
             arquivo.write(str(matriz[i, j]))
             arquivo.write(" ")
         arquivo.write("\n")
-
-    arquivo.write("\n\n")
-
-    # Matriz de cenario
-    arquivo.write("Matriz de Cenário: \n")
-    for i in range(x+2):
-        for j in range(y+2):
-            arquivo.write(str(matriz_cenario[i, j]))
-            arquivo.write(" ")
-        arquivo.write("\n")
-            
+    
     arquivo.write("\nCaminho: " + str(caminho))
     
     arquivo.close()
@@ -106,15 +167,12 @@ if __name__ == '__main__':
     y = int(input("Determina o y da matriz: "))
 
     # Cria a matriz que será o cenario do jogo
-    matriz_cenario = np.zeros((x,y))
+    matriz_cenario = (np.zeros((x,y)))
 
     # Cria as classes da matrizes
     personagem = Luffy(x, y)
     tesouro = OnePiece(x, y)
     navio = Navio(x, y, matriz_cenario)
-
-    # Cria o void do mapa com -1
-    matriz_cenario = np.pad(matriz_cenario, pad_width=1, mode='constant', constant_values=-1)
 
     os.system('clear') or None
 
@@ -128,74 +186,71 @@ if __name__ == '__main__':
     for i in range(x):
         for j in range(y):
             matriz[i, j] = "{:.2f}" .format(calculaEstimativa(i, j))
-            
-    matriz_cenario[personagem.posicaoX, personagem.posicaoY] = 10
-    matriz_cenario[tesouro.posicaoX, tesouro.posicaoY] = 100
+    
+    # Adiciona os barcos nas posições dentro da matriz de estimativas
+    for i in range(x):
+        for j in range(y):
+            if(matriz_cenario[i, j] == -1):
+                matriz[i, j] = 700.0
+    
+    matriz_cenario[personagem.posicaoX, personagem.posicaoY] = 15
+    matriz_cenario[tesouro.posicaoX, tesouro.posicaoY] = 99
 
     # Inicia de fato o programa
     # Fila de posição do caminho
-    fila = []
-    fila_aux = []
-    
-    # Caminho que o personagem ira percorrer
+    fila_valores = []
+    fila_posicoes = []
     caminho = []
     
     # Aonde o personagem começa
     luffy = personagem.posicaoX, personagem.posicaoY
     one_piece = tesouro.posicaoX, tesouro.posicaoY
 
-    # Laço para encontrar o One Piece
     if(luffy == one_piece):
         print('O One Piece estava do seu lado o tempo todo')
+        exit() # Sai direto do programa
     else:
         while (luffy != one_piece):
             
-            # Pega a posição do personagem
-            posicaoPersonagem(x ,y, fila, personagem.posicaoX, personagem.posicaoY)
-
-            # Usa a posição do personagem para pegar na matriz de custo
-            custoMovimentacao(fila, fila_aux, matriz)
+            # Pega a posição do personagem e o valor que custara está movimentação 
+            posicaoPersonagem(fila_posicoes, personagem.posicaoX, personagem.posicaoY)
+            movimentacaoPersonagem(matriz, fila_posicoes, fila_valores, personagem.posicaoX, personagem.posicaoY)
             
-            # Ordena a lista de numeros
-            fila_aux = sorted(fila_aux)
+            # Ordena os dois vetores com base no primeiro
+            fila_valores, fila_posicoes = ordena(fila_valores, fila_posicoes)
             
             # Testa se a fila esta vazia
-            if (fila == []):
+            if (fila_posicoes == []):
                 break
             
-            # Atribui a nova posicao para o personagem
-            personagem.posicaoX = fila[0][0]
-            personagem.posicaoY = fila[0][1]
+            # Primeiro print, aonde o luffy começara e aonde o one piece está
+            printaTerminal(personagem.posicaoX, personagem.posicaoY, tesouro.posicaoX, tesouro.posicaoY, fila_valores, fila_posicoes, matriz, matriz_cenario)
             
-            #print(personagem.posicaoX, personagem.posicaoY)
+            # Adiciona o caminho percorrido
+            caminho.append(luffy)
             
-            # Atribui um valor que nao podera voltar
-            matriz[personagem.posicaoX, personagem.posicaoY] = 999
+            # Atribui um valor para o boneco não retornar
+            matriz[personagem.posicaoX, personagem.posicaoY] = 800.0
             
-            # Recebe a nova posição para as comparações
+            # Atribui a proxima posição nas coordenadas
+            personagem.posicaoX = fila_posicoes[0][0]
+            personagem.posicaoY = fila_posicoes[0][1]
             luffy = personagem.posicaoX, personagem.posicaoY
             
-            # Adiciona o caminho que esta sendo percorrido
-            caminho.append(fila[0])
-            #print(caminho)
-                
-            #variavel += 1    
-            fila.clear()
+            # Move o personagem para a nova posição
+            matriz_cenario[personagem.posicaoX, personagem.posicaoY] = 15
+            
+            # Mesmo print, mas agora com o personagem avançado na matriz
+            #printaTerminal(personagem.posicaoX, personagem.posicaoY, tesouro.posicaoX, tesouro.posicaoY, fila_valores, fila_posicoes, matriz, matriz_cenario)
+            
+            # Zera as filas para não ter lixo no buffer
+            fila_posicoes.clear()
+            fila_valores.clear()
+            
+    if(luffy == one_piece):
+        print("O luffy finalmente encontrou o One Piece, seu caminho foi esse: ", caminho)
+
+    escreveArquivo(x, y, matriz, matriz_cenario, caminho, personagem.posicaoX, personagem.posicaoY, tesouro.posicaoX, tesouro.posicaoY)
+        
 
             
-    escreveArquivo(x, y, matriz, matriz_cenario, caminho,
-                   personagem.posicaoX, personagem.posicaoY,
-                   tesouro.posicaoX, tesouro.posicaoY)
-
-
-    #print('Perosnagem: ', personagem.posicaoX, personagem.posicaoY)
-    #print('Tesouro: ' , tesouro.posicaoX, tesouro.posicaoY)
-    #print('\n')
-    #print(matriz)
-    #print('\n')
-    #print(matriz_cenario)
-    #print('\n')
-    
-    #print(fila)
-    #print(fila[3])
-  
