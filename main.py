1b9e18da5e09ee146a320b6231cd3af14b9ea332
Gabriel Mazzuco <gabrielalvesmazzuco@hotmail.com>
2022-12-06 19:02:51 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,6 @@
         # 800.0 = caminhos já percorridos
         # 999.9 = void do mapa -> "valor teorico que não aparece de fato no mapa"
         
-
 import numpy as np
 import math
 import random
@@ -29,16 +28,29 @@ class OnePiece:
         self.posicaoY = random.randint(0, y-1)
         
 class Navio:
-    def __init__(self, x, y, matriz_cenario):
+    def __init__(self, x, y, matriz_cenario, dificuldade):
         for i in range(Navio.Barcos()):
             matriz_cenario[random.randint(0, x-1), random.randint(0, y-1)] = -1
             
         
-    # Define quantos barcos apareceram no mapa
+    # Define quantos barcos apareceram no mapa, para aumentar a dificuldade, é so mudar o valor da divisão
     def Barcos():
-        aux = x * y / 8 
+        aux = x * y / Navio.trueDificuldade()
         aux = math.ceil(aux) 
         return aux 
+    
+    def trueDificuldade():
+        if(dificuldade == 1):
+            return 8
+            
+        elif(dificuldade == 2):
+            return 4
+            
+        elif(dificuldade == 3):
+            return 2
+            
+        elif(dificuldade == 4):
+            return 1
     
 def calculaEstimativa(posicaoX, posicaoY):
     if (matriz[posicaoX, posicaoY] == -1):
@@ -115,8 +127,17 @@ def movimentacaoPersonagem(matriz, fila_posicoes, fila_valores, posicaoX, posica
         aux = matriz[posicaoX, posicaoY + 1]
         fila_valores.append(aux) 
     
-    return fila_valores
+    return fila_valores           
 
+def verificaObstrucao(fila_valores):
+    if((fila_valores[3] == 700.0) or (fila_valores[3] == 800.0) or (fila_valores[3] == 999.9)):
+        if((fila_valores[2] == 700.0) or (fila_valores[2] == 800.0) or (fila_valores[2] == 999.9)):
+            if((fila_valores[1] == 700.0) or (fila_valores[1] == 800.0) or (fila_valores[1] == 999.9)):
+                if((fila_valores[0] == 700.0) or (fila_valores[0] == 800.0) or (fila_valores[0] == 999.9)):
+                    return True
+    else:
+        return False
+    
 def printaTerminal(iteracao, personagem_posicaoX, personagem_posicaoY, tesouro_posicaoX, tesouro_posicaoY,
                    fila_valores, fila_posicoes, matriz, matriz_cenario):
     
@@ -168,6 +189,25 @@ if __name__ == '__main__':
     print("Busca do primeiro melhor, determine a matriz grid:\n")
     x = int(input("Determina o x da matriz: "))
     y = int(input("Determina o y da matriz: "))
+    
+    os.system('clear')
+    
+    # Seleção da dificuldade do jogo
+    while True:
+        print('''Dificuldade do jogo: 
+          1 - Fácil
+          2 - Médio
+          3 - Díficil
+          4 - Muito Díficil''')
+        
+        dificuldade = (int(input('Escolha uma opção: ')))
+        
+        if((dificuldade > 4) or (dificuldade < 1)):
+            print("Opção inválida, escolha novamente")
+        else:
+            break
+    
+    os.system('clear')
 
     # Cria a matriz que será o cenario do jogo
     matriz_cenario = (np.zeros((x,y)))
@@ -175,7 +215,7 @@ if __name__ == '__main__':
     # Cria as classes da matrizes
     personagem = Luffy(x, y)
     tesouro = OnePiece(x, y)
-    navio = Navio(x, y, matriz_cenario)
+    navio = Navio(x, y, matriz_cenario, dificuldade)
 
     os.system('clear') or None
 
@@ -229,6 +269,10 @@ if __name__ == '__main__':
             # Testa se a fila esta vazia
             if (fila_posicoes == []):
                 break
+            
+            if(verificaObstrucao(fila_valores) == True):
+                print('Luffy se perdeu e não achou os Road Poneglyph!!!')
+                exit()
             
             # Primeiro print, aonde o luffy começara e aonde o one piece está
             printaTerminal(iteracao, personagem.posicaoX, personagem.posicaoY, tesouro.posicaoX, tesouro.posicaoY, fila_valores, fila_posicoes, matriz, matriz_cenario)
